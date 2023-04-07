bdbb59b2b896271ea0636025ab08bc108ff4ce0c
Shubham-andy7 <67570264+Shubham-andy7@users.noreply.github.com>
2023-04-07 05:42:11 +0530

    Updates simple parser

--- bc.py
+++ bc.py
@@ -1,12 +1,12 @@
 class Parser(object):
     def __init__(self, expression):
         self.expression = expression
-
     def execute(self):
         """
         This method parses given expression and returns list of items
         """
-        pass
+        self.expression = self.expression.split('\n')
+        return self.expression
 
     # More functions to follow
 
@@ -15,11 +15,26 @@ class Evaluator(object):
     def __init__(self, items, variables):
         self.items = items
         self.variables = variables
-
     def execute(self):
         """
         This method evaluates given list of items and returns result
         """
+        # print(self.items)
+        for statements in self.items:
+          if statements.startswith("print "):
+            expression = statements[6: ].strip()
+            val = eval(expression, self.variables)
+            return val
+          else:
+            statements = statements.replace(' ', '')
+            calculate = statements.split('=')
+            variable = calculate[0]
+            expression = calculate[1]
+            try:
+              val = eval(expression, self.variables)
+            except NameError:
+              return "NameError"
+            self.variables[variable] = val
         pass
 
     # More functions to follow
@@ -35,12 +50,20 @@ class ExpressionEvaluator(object):
         """
         This method parses given expression, evaluates it and returns the final result
         """
-        pass
+        parsedlist = self.expression.split('\n')
+        return parsedlist
 
     # More functions to follow
 
 
 if __name__ == '__main__':
-    expression = '1 + 2'
-    evaluator = ExpressionEvaluator(expression)
-    print(evaluator.execute())
+  lines = ""
+  while True:
+    line = input()
+    if line:
+      lines += line + '\n'
+    else:
+      break
+  parser = Parser(lines)
+  evaluator = Evaluator(parser.execute(), {})
+  print(evaluator.execute())
